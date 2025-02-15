9eabf85077f6c8ee07d0c5d52eafbc3227fbe886
Yashaswani Choudhary <yashaswanichoudhary9@gmail.com>
2025-02-15 22:55:53 +0530

    Restructured src directory: moved files into utils, services, styles, and hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
-import reportWebVitals from "./utils/reportWebVitals";
+import reportWebVitals from './utils/reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from "./context/ThemeContext";
 
